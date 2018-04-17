58c95d761f306892a69700f4ec8aef8a2006210b
rloizzo <rloizzo@nd.edu>
2018-04-17 01:31:00 -0400

    implement expected points algorithm

--- classifiers.py
+++ classifiers.py
@@ -13,6 +13,61 @@ from sklearn import cluster
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def get_bin(field_pos):
+    b = 3
+    poss_bins = []
+    while b < 100:
+        poss_bins.append(b)
+        b += 5
+    # find the bin that is closest to the field position in question
+    poss_bins = map(lambda x: x - field_pos, poss_bins)
+    for val in poss_bins:
+        if abs(val) <= 2:
+            return val + field_pos
+
+def get_class_decision(field_pos, exp_pts_bins, counts_bins):
+    b = get_bin(field_pos)
+    # determine if one class is overwhelming majority
+    # i = 0: punt
+    if counts_bins[b][1] + counts_bins[b][2] > 0:
+        ratio = float(counts_bins[b][0]) / float(counts_bins[b][1] + counts_bins[b][2])
+        if ratio >= 10:
+            return 1
+    else:
+        if counts_bins[b][0] > 0:
+            return 1
+        else:
+            return None # all 3 have count 0
+    # i = 1: FG
+    if counts_bins[b][0] + counts_bins[b][2] > 0:
+        ratio = float(counts_bins[b][1]) / float(counts_bins[b][0] + counts_bins[b][2])
+        if ratio >= 10:
+            return 2
+    else:
+        if counts_bins[b][1] > 0:
+            return 2
+        else:
+            return None # all 3 have count 0
+    # i = 2: offensive play
+    if counts_bins[b][0] + counts_bins[b][1] > 0:
+        ratio = float(counts_bins[b][2]) / float(counts_bins[b][0] + counts_bins[b][1])
+        if ratio >= 10:
+            return 3
+    else:
+        if counts_bins[b][2] > 0:
+            return 3
+        else:
+            return None # all 3 have count 0
+    # if no class has majority, return class with highest expected points
+    if exp_pts_bins[b][0] > exp_pts_bins[b][1] and exp_pts_bins[b][0] > exp_pts_bins[b][2]:
+        return 1
+    elif exp_pts_bins[b][1] > exp_pts_bins[b][0] and exp_pts_bins[b][1] > exp_pts_bins[b][2]:
+        return 2  
+    elif exp_pts_bins[b][2] > exp_pts_bins[b][0] and exp_pts_bins[b][2] > exp_pts_bins[b][1]:
+        return 3
+    else:
+        return None
+
 def find_dist(point, centroid):
     running_sum = 0.0
     for i in range(len(point)):
@@ -66,6 +121,36 @@ if __name__ == '__main__':
     X_headers = list(data)[:5]
     data = data.as_matrix()
 
+    # create dictionary of 5 yard bins for every possible field position
+    # field_pos_bins = {bin: [pts from punts, pts from FG, pts from off plays]}
+    exp_pts_bins = {}
+    counts_bins = {}
+    b = 3
+    while b < 100:
+        exp_pts_bins[b] = [0,0,0]
+        counts_bins[b] = [0,0,0]
+        b += 5
+
+    # get the average points earned/lost after each play type in each bin
+    for line in data:
+        b = get_bin(line[1])
+        Class = int(line[5] - 1)
+        exp_pts_bins[b][Class] += line[4]
+        counts_bins[b][Class] += 1
+
+    for b in exp_pts_bins.keys():
+        for i in range(3):
+            if counts_bins[b][i] != 0:
+                exp_pts_bins[b][i] /= float(counts_bins[b][i])
+  
+    cccc = 0
+    for line in data:
+        b = get_bin(line[1])
+        new_class = get_class_decision(b,exp_pts_bins,counts_bins)
+        if new_class != None: 
+            line[5] = new_class
+        #otherwise, class remains unchanged
+
     X = data[:,:5]
     y = data[:,5]
 
@@ -110,5 +195,4 @@ if __name__ == '__main__':
     print cluster1_counts
     print cluster2_counts
     print cluster3_counts
-        
-            
+
